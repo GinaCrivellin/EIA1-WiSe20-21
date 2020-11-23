ee1f81b141f9973571fe5bd088806614ed786ed1
Gina <gina.crivellin@gmail.com>
2020-11-23 13:41:21 +0100

    Clickevent

--- Steckbrief/06/06_script.ts
+++ Steckbrief/06/06_script.ts
@@ -3,39 +3,41 @@
     var Europa18: number = 4209.3;
     var Australien18: number = 2100.5;
     var Asien18: number = 16274.1;
-    var SüdAmerica18: nmber = 1261.5;
+    var SüdAmerica18: number = 1261.5;
     var NordAmerica18: number = 6035.6;
     var Africa18: number = 1235.5;
 
     var Europa08: number = 4965.7;
     var Australien08: number = 1993;
     var Asien08: number = 12954.7;
-    var SüdAmerica08: nmber = 1132.6;
+    var SüdAmerica08: number = 1132.6;
     var NordAmerica08: number = 6600.4;
     var Africa08: number = 1028;
 
-    var Total: number = Europa18+Australien18+Asien18+SüdAmerica18+NordAmerica18+Africa18;
+    var Total: number = (Europa18+Australien18+Asien18+SüdAmerica18+NordAmerica18+Africa18);
 
-    var europeRel: number = Europa18/Total*100 + "%";
-    var australiaRel: number = Australien18/Total*100;
-    var asiaRel: number = Asien18/Total*100;
-    var southAmericaRel: number = SüdAmerica18/Total*100;
-    var northAmericaRel: number = NordAmerica18/Total*100;
-    var africaRel: number = Africa18/Total*100;
+    console.log(Total)
 
-    var europeGrow: number = Europa08/Europa18 + "%";
-    var australiaGrow: number = Australien08/Australien18 + "%";
-    var asiaGrow: number = Asien08/Asien18 + "%";
-    var southAmericaGrow: number = SüdAmerica08/SüdAmerica18 + "%";
-    var northAmericaGrow: number = NordAmerica08/NordAmerica18 + "%";
-    var africaGrow: number = Africa08/Africa18 + "%" ;
+    var europeRel: number = Math.round(Europa18/Total*100)+ "%";
+    var australiaRel: number = Math.round(Australien18/Total)*100 +"%";
+    var asiaRel: number = Math.round(Asien18/Total*100) + "%";
+    var southAmericaRel: number = Math.round(SüdAmerica18/Total*100) + "%";
+    var northAmericaRel: number = Math.round(NordAmerica18/Total*100) + "%";
+    var africaRel: number = Math.round(Africa18/Total*100) + "%";
 
-    var europeMinus: number = Europa18-Europa08;
-    var australiaMinus: number = Australien18-Australien08;
-    var asiaMinus: number = Asien18-Asien08;
-    var southAmericaMinus: number = SüdAmerica18-SüdAmerica08;
-    var northAmericaMinus: number = NordAmerica18-NordAmerica08;
-    var africaMinus: number = Africa18-Africa08;
+    var europeGrow: number = Math.round(Europa08/Europa18*100) + "%";
+    var australiaGrow: number = Math.round(Australien08/Australien18*100) + "%";
+    var asiaGrow: number = Math.round(Asien08/Asien18) + "%";
+    var southAmericaGrow: number = Math.round(SüdAmerica08/SüdAmerica18*100) + "%";
+    var northAmericaGrow: number = Math.round(NordAmerica08/NordAmerica18*100) + "%";
+    var africaGrow: number = Math.round(Africa08/Africa18*100) + "%" ;
+
+    var europeMinus: number = Math.round(Europa18-Europa08);
+    var australiaMinus: number = Math.round(Australien18-Australien08);
+    var asiaMinus: number = Math.round(Asien18-Asien08);
+    var southAmericaMinus: number = Math.round(SüdAmerica18-SüdAmerica08);
+    var northAmericaMinus: number = Math.round(NordAmerica18-NordAmerica08);
+    var africaMinus: number = Math.round(Africa18-Africa08);
 
 
 
