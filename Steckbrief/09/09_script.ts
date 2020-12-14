0f9ef6597a0a8da94fa3d993cb2e72fc40a6e3e0
Gina <gina.crivellin@gmail.com>
2020-12-14 11:00:18 +0100

    neu

---
+++ Steckbrief/09/09_script.ts
@@ -0,0 +1,28 @@
+
+// Leere Liste
+var list : string [] = [];
+
+// Variable, die das eingegeben aus dem HTML aufnimmt
+var input = document.getElementById('myInput');
+
+// Funktion, die das aufgenommene Element pusht
+ function addToList (event){
+    console.log(event)
+    list.push(event);
+}
+
+// Event, dass bei Input die Funktion aufruft
+input.addEventListener("input", addToList)
+
+// Liste wird in Konsole ausgegeben
+console.log(list)
+
+
+
+
+
+ //   input.addEventListener("input", (event) => {
+ //       console.log("Input received: ", event.target.value);
+ //   }
+ //   )
+
