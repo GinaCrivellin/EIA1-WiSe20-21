42f98639e14b8424f7fbcd077cf6129e0ec91dca
Gina <gina.crivellin@gmail.com>
2020-12-16 12:30:45 +0100

    dnjdsjsd

--- Steckbrief/09/09_script.js
+++ Steckbrief/09/09_script.js
@@ -1,3 +1,4 @@
+var _this = this;
 // Variable, die das eingegeben aus dem HTML aufnimmt
 var input = document.getElementById("myInput");
 // Funktion, die den Text in ein Element packt
@@ -5,17 +6,35 @@ function addToHTML() {
     // Derzeitiges Element festlegen
     var currentElement = document.getElementById("myInput").value;
     // Variable, die neues Element erschafft
-    var newElement = document.createElement("Task");
-    // Neues Element zum Body hinzufügen
-    document.body.appendChild(newElement);
+    var newElement = document.createElement("li");
+    var newCheckbox = document.createElement("checkbox");
     // Neues Element wird gleich dem Inhalt des Alten gesetzt
     newElement.innerHTML = currentElement;
-    console.log(currentElement);
+    newElement.className = "Task";
+    // Neues Element zum Body hinzufügen
+    document.body.appendChild(newElement);
+    newElement.appendChild(newCheckbox);
+    console.log(newCheckbox);
 }
-// Event, dass bei Enter press die Funktion aufruft
+// Löscht Zeile nach Enter
+function clear() {
+    document.getElementById("myInput").value = "";
+}
+// Index
+var index = 0;
+// Counter
+function counter() {
+    document.querySelector("#counter").innerHTML = index + " open tasks";
+}
+counter();
+// Event, dass bei Enter press die Funktion aufruft+Zeile cleart+count anführt
 input.addEventListener("keypress", function (e) {
     if (e.code === "Enter") {
         addToHTML();
+        clear();
+        _this.currentElement = null;
+        index++;
+        counter();
     }
 });
 //# sourceMappingURL=09_script.js.map
--- Steckbrief/09/09_script.ts
+++ Steckbrief/09/09_script.ts
@@ -8,21 +8,45 @@ function addToHTML (): void {
     var currentElement: string = (<HTMLInputElement>document.getElementById("myInput")).value;
 
     // Variable, die neues Element erschafft
-    var newElement: HTMLElement = document.createElement("Task");
-
-    // Neues Element zum Body hinzufügen
-    document.body.appendChild(newElement);
+    var newElement: HTMLElement = document.createElement("li");
+    
+    var newCheckbox: HTMLElement = document.createElement("checkbox");
 
     // Neues Element wird gleich dem Inhalt des Alten gesetzt
     newElement.innerHTML = currentElement;
+    newElement.className = "Task";
 
-    console.log(currentElement);
+    // Neues Element zum Body hinzufügen
+    document.body.appendChild(newElement);
+    newElement.appendChild(newCheckbox);
+
+    console.log(newCheckbox);
 }
 
-// Event, dass bei Enter press die Funktion aufruft
+
+// Löscht Zeile nach Enter
+function clear(): void {
+    (<HTMLInputElement>document.getElementById("myInput")).value = "";
+}
+
+// Index
+var index: number = 0;
+
+// Counter
+function counter(): void {
+    document.querySelector("#counter").innerHTML = index + " open tasks";
+}
+
+counter();
+
+// Event, dass bei Enter press die Funktion aufruft+Zeile cleart+count anführt
 input.addEventListener("keypress", (e) => {
     if (e.code === "Enter") {
         addToHTML();
+        clear();
+        this.currentElement = null;
+        index++;
+        counter();
     }
 
 }
